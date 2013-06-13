15f886f73ab29143831070fc9cb317844d16069e
sony <sony@vaio.(none)>
2013-06-13 21:08:50 +0530

    inner pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pages::Application.config.secret_token = 'ad05025ad8471e6cecd00f03d431554109eb2001224d72c125af5a9ce32532ad93f7b66ea291dce84546337c9caca5de137d61618860cdfc4eac8a4728aa4229'
